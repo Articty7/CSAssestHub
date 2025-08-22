a30637b633babef2848e57bb6699e84489d2fc49
Malachi Maynard <malachimaynard@Malachis-MacBook-Air.local>
2025-08-22 16:10:17 -0700

    Use psycopg2-binary; install deps during build

--- render-build.sh
+++ render-build.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 set -e
+
+# Install Python deps
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+
+# Build the frontend
 pushd react-vite
 npm ci
 npm run build
 popd
-# DO NOT run flask db upgrade here
