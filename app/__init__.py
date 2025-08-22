d5b057daf47078abef5c6fc581ed1a3339fab924
Malachi Maynard <malachimaynard@Malachis-MacBook-Air.local>
2025-08-22 07:37:44 -0700

    fix minor error

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,8 @@ from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect, generate_csrf
 from flask_login import LoginManager
 from dotenv import load_dotenv
+from sqlalchemy import text
+
 
 from .models import db, User
 from .api.user_routes import user_routes
